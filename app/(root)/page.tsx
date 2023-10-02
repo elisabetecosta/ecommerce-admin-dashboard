d02af371abdae11ce520e8ac593dcb782981803f
Elisabete Costa <elisabete_costa94@hotmail.com>
2023-10-02 23:39:26 +0100

    setup auth with clerk

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,9 @@
-export default function Home() {
+import { UserButton } from "@clerk/nextjs";
+
+export default function SetupPage() {
   return (
-    <p>Hello Admin Dashboard</p>
+    <div className="p-4">
+      <UserButton afterSignOutUrl="/" />
+    </div>
   )
 }
