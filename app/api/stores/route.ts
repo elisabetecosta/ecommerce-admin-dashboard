8dbea352f0a57e5629a11746abbee0651019b230
Elisabete Costa <elisabete_costa94@hotmail.com>
2023-10-17 19:38:53 +0100

    change no userId message to unauthenticated

--- app/api/stores/route.ts
+++ app/api/stores/route.ts
@@ -13,7 +13,7 @@ export async function POST(
         const { name } = body;
 
         if (!userId) {
-            return new NextResponse("Unauthorized", { status: 401 });
+            return new NextResponse("Unauthenticated", { status: 401 });
         }
 
         if (!name) {
