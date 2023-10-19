27cf99b6591546a82cb82e7087853a31bb77a841
Elisabete Costa <elisabete_costa94@hotmail.com>
2023-10-19 18:41:07 +0100

    add products entity

---
+++ app/(dashboard)/[storeId]/(routes)/products/components/product-client.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { useParams, useRouter } from "next/navigation";
+import { Plus } from "lucide-react";
+
+import { ProductColumn, columns } from "./columns";
+import { Heading } from "@/components/ui/heading";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { DataTable } from "@/components/ui/data-table";
+import { ApiList } from "@/components/ui/api-list";
+
+interface ProductClientProps {
+    data: ProductColumn[]
+};
+
+
+const ProductClient: React.FC<ProductClientProps> = ({
+    data
+}) => {
+
+    const params = useParams();
+    const router = useRouter();
+
+    return (
+        <>
+            <div className="flex items-center justify-between">
+                <Heading 
+                    title={`Products (${data.length})`}
+                    description="Manage products for your store"
+                />
+                <Button onClick={() => router.push(`/${params.storeId}/products/new`)}>
+                    <Plus className="mr-2 h-4 w-4" />
+                    Add New
+                </Button>
+            </div>
+            <Separator />
+
+            <DataTable filterKey="name" columns={columns} data={data} />
+
+            <Heading title="API" description="API calls for Products" />
+            <Separator />
+
+            <ApiList entityName="products" entityIdName="productId" />
+        </>
+    );
+}
+
+export default ProductClient;
